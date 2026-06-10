99c8ca53a95db8fbe56a927b26f1553fee5dc938
windows_on_wsl <otomijuf.004@gmail.com>
2026-06-10 17:04:02 +0900

    typo

--- release.sh
+++ release.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
-cd themes/myhabor/
-git checkout book_imp &&\
-npm run build-prod &&\
-git add static &&\
-git commit -m "build" &&\
-git push
-
-cd ../../
+# cd themes/myhabor/
+# git checkout book_imp &&\
+# npm run build-prod &&\
+# git add static &&\
+# git commit -m "build" &&\
+# git push
+#
+# cd ../../
 hugo
 git add docs/
 git commit -m "build"
